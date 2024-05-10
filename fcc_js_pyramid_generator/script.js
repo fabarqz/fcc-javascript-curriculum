a9cb7a21532e4242d7daa292edc11517c6f41b16
franz abarquez <franzdale.abarquez@gmail.com>
2024-05-10 14:46:12 +0800

    progressed with the lecture

--- fcc_js_pyramid_generator/script.js
+++ fcc_js_pyramid_generator/script.js
@@ -1,3 +1,19 @@
 const character = "#";
 const count = 8;
 const rows = [];
+
+for(let i=0;i<count;i++){
+  rows.push(character);
+
+}
+
+console.log(rows)
+
+let result="";
+
+for (const row of rows) {
+  result+=row+"\n";
+  
+}
+
+console.log(result);

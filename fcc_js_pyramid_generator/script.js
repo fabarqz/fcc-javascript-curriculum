649bbf7070eff265ec406c52f66f4e6cf17bf87c
franz abarquez <franzdale.abarquez@gmail.com>
2024-05-12 21:55:26 +0800

    added progress for the fcc_js_pyramid_generator

--- fcc_js_pyramid_generator/script.js
+++ fcc_js_pyramid_generator/script.js
@@ -1,19 +1,47 @@
+
 const character = "#";
 const count = 8;
 const rows = [];
 
-for(let i=0;i<count;i++){
-  rows.push(character);
-
-}
-
-console.log(rows)
-
 let result="";
 
-for (const row of rows) {
-  result+=row+"\n";
-  
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount-rowNumber) + character.repeat(rowNumber) + " ".repeat(rowCount-rowNumber);
 }
 
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(padRow(i + 1, count));
+}
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+
+
 console.log(result);
+
+
+
+function pyramid(){
+  const character = "#";
+  const count = 8;
+  const rows = [];
+  let result="";
+
+  for(let i=0;i<count;i++){
+    rows.push(character.repeat(i+1));
+  
+  }
+
+  for (const row of rows) {
+    result+=row+"\n";
+    
+  }
+  
+  return result;
+}
+
+let pyr=pyramid()
+
+
